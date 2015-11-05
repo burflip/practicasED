24c47b2cc89503406609adc9cc1829cf88163b6a
martaarenas <martilla_311@hotmail.com>
2015-11-05 13:43:51 +0100

    Update fecha.h

--- fecha.h
+++ fecha.h
@@ -1,10 +1,11 @@
 
-#ifndef __FECHA_H
-#define __FECHA_H
+#ifndef FECHA
+#define FECHA
 
+#include <iostream>
 #include <string>
-#include <iostream> 
 
+using namespace std;
 
 //! Clase fecha, asociada a la 
 /*! fecha::fecha,  .....
@@ -13,7 +14,6 @@
  @todo Implementar esta clase
 
 */
-using namespace std;
 
 class fecha {
 
@@ -21,9 +21,19 @@ public:
  fecha (); //Constructor de fecha por defecto
  fecha (const string & s); // s es un string con el formato mm/dd/aaaa  hh:mm:ss AM/PM
 
+ void stringToFecha(const string & x);
+ string toString() const;
+
+ int getYear();
+ int getMonth();
+ int getDay();
+ int getHour();
+ int getMinute();
+ int getSec();
+
  fecha & operator=(const fecha & f);
  fecha & operator=(const string & s); // s es un string con el formato mm/dd/aaaa hh:mm:ss AM/PM
- string toString() const; //convierte la fecha a un string
+ 
 
 // Operadores relacionales
  bool operator==(const fecha & f) const;
@@ -32,13 +42,7 @@ public:
  bool operator<=(const fecha & f)const;
  bool operator>=(const fecha & f) const;
  bool operator!=(const fecha & f)const;
- //Getters
-  int getSec() const;
-  int getMin() const;
-  int getHour() const;
-  int getDay() const;
-  int getMon() const;
-  int getYear() const;
+
 
 private:
   int  sec;   // seconds of minutes from 0 to 61
@@ -47,20 +51,14 @@ private:
   int  mday;  // day of month from 1 to 31
   int  mon;   // month of year from 0 to 11
   int  year;  // year since 2000
-              
-  void parseString(const string & s);
-  
 
-friend ostream& operator<< ( ostream& os, const fecha & f);
-friend ostream& operator<< ( ostream& os, const fecha * f);
+ friend  ostream& operator<< ( ostream& os, const fecha & f); 
 };
 
 /* @brief imprime fecha con el formato  mm/dd/aaaa hh:mm:ss AM/PM
 */
- ostream& operator<< ( ostream& os, const fecha & f);
- ostream& operator<< ( ostream& os, const fecha * f); 
+// ostream& operator<< ( ostream& os, const fecha & f); 
 
-#include "fecha.hxx" // Incluimos la implementacion.
-
+#include "fecha.hxx"
 
 #endif
